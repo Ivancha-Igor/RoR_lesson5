9a9b73a42a13c3a6e0b6713c9c1b83ad5d60072c
Ivancha-Igor <ivan4afamily@gmail.com>
2014-11-21 19:23:35 +0200

    Minor changes in validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title, :body
   validates_uniqueness_of :title
-  validates_length_of :title, in: 5..100
+  validates_length_of :title, {minimum: 3}
 
   belongs_to :user
 end
